f5677be2be7a2d8321b3faeec3f308313c0ada31
JuanFuriaz <juan.furiaz88@gmail.com>
2020-05-21 10:44:25 +0200

    Experiments Buffer 5000

---
+++ ExpInfo.sh
@@ -0,0 +1,1 @@
+python train.py --infomax t --ndim 64 --log-interval 40000 --seed 0 --buffer 5000 --latent-train --tb --vis --freeze --title infob5000r0 --rl-path contin_infomax/pretrained_infomax_64_stack4_earlystop_conti_nonexpert_frameskip1.ckpt
